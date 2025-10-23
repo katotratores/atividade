1b75a4a86305ae749f63d701543ab9fa12b305dc
EmoMaisFracoDoPolo <guilherme.candido.farias@escola.pr.gov.br>
2025-10-23 07:51:44 -0300

    criei minha tarefa

---
+++ 1a.js
@@ -0,0 +1,5 @@
+let numero = 6;
+
+if (numero >= 0) {
+    console.log ("É positivo");
+}
---
+++ 3a.js
@@ -0,0 +1,8 @@
+let nota = 75
+if (nota >= 80){
+    console.log('nota a')
+} else if (nota>=60){
+    console.log('nota b')
+} else {
+    console.log(' nota c')
+}
